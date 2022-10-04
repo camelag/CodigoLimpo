c36f731f2d2ad996681305a209d1252012acee43
Camila Garcia <camila142garcia@gmail.com>
2022-10-04 01:08:06 -0300

    🎨 Código mais Limpo

--- exercicioantes.js
+++ exercicioantes.js
@@ -1,12 +1,22 @@
-var calcular = (e , t , j ,p) => { 
-    j = j > 1 ? j / 100 : j
-    var f = t - e
-    return e + (f * Math.pow( 1 + j, p) )
+/**
+ * Calculo de contrato de veiculo com juros
+ * @param Entrada Entrada do cliente
+ * @param totalVeiculo Valor total do veiculo
+ * @param Juros Juros aplicados ao mes
+ * @param parcelas 
+ * @returns Retorna o valor financiado
+ */
+
+
+var CalcularContratoVeiculo = (Entrada, totalVeiculo, Juros, parcelas) => {
+    Juros = Juros > 1 ? Juros / 100 : Juros
+    var Financiar = totalVeiculo - Entrada
+    return Entrada + (Financiar * Math.pow(1 + Juros, parcelas))
 }
 
-var e = 1
-var t = 2
-var j = 3
-var p = 4
+var entrada = 1
+var totalVeiculo = 2
+var Juros = 3
+var parcelas = 4
 
-console.log(calcular(e, t, j, p))
+console.log(CalcularContratoVeiculo(entrada, totalVeiculo, Juros, parcelas))
